c29168b6dbd6d2e254b8e71bc93f4a9f823415f6
lucassaraivalara <lucassl.sistemas@gmail.com>
2026-08-05 11:02:53 -0300

    chore: criar projeto Spring Boot inicial

---
+++ backend/novexa-api/novexa-api/src/main/java/br/com/novexa/erp/NovexaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.novexa.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovexaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovexaApiApplication.class, args);
+    }
+
+}
---
+++ backend/novexa-api/novexa-api/src/test/java/br/com/novexa/erp/NovexaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.novexa.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovexaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
